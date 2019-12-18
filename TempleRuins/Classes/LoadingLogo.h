742a88e4def1c4c27c09dda1479636c0ecfd65be
ngocbuii26898 <ngocbui26898@gmail.com>
2019-12-18 13:34:43 +0700

    fisrt push in ngoc branch

--- TempleRuins/Classes/LoadingLogo.h
+++ TempleRuins/Classes/LoadingLogo.h
@@ -6,11 +6,19 @@
 
 class LoadingLogo : public cocos2d::Scene
 {
+private:
+    int timeRemain;
+    Scene* scene;
+    Label* label_Timer;
 public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
-    
+    void Init();
+    void Update(float deltaTime);
+    Sprite* Clone(Sprite* sprite);
+    CountTimer(Scene* scene, int timeRemain);
+    ~CountTimer();
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     void changeLoading(float dt);
--- TempleRuins/Classes/MiniGame.h
+++ TempleRuins/Classes/MiniGame.h
@@ -5,7 +5,7 @@
 
 #define NUM_ITEM 10
 #define TIME_REMAIN 30
-
+using namespace cocos2d;
 class MiniGame : public Scene
 {
 private:
@@ -16,8 +16,13 @@ private:
 public:
 	static Scene* createMiniGame();
 	virtual bool init();
+	bool onTouchBegan(Touch* touch, Event* event);
+	bool onTouchEnded(Touch* touch, Event* event);
+	bool onTouchMoved(Touch* touch, Event* event);
 	MiniGame();
 	~MiniGame();
+
+	
 	bool OnTouhBegan(Touch* touch, Event* event);
 	void update(float deltaTime);
 	CREATE_FUNC(MiniGame);
